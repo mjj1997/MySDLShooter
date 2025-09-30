21ebae8799255125e143fd10f0d4fb67b22ffd6d
mjj1997 <45837504+mjj1997@users.noreply.github.com>
2025-09-30 23:05:16 +0800

    build: Add SDL2 libraries using vcpkg

--- Dependencies.cmake
+++ Dependencies.cmake
@@ -8,5 +8,9 @@ function(MySDLShooter_setup_dependencies)
   # For each dependency, see if it's
   # already been provided to us by a parent project
 
+  find_package(SDL2 CONFIG REQUIRED)
+  find_package(SDL2_image CONFIG REQUIRED)
+  find_package(SDL2_mixer CONFIG REQUIRED)
+  find_package(SDL2_ttf CONFIG REQUIRED)
 
 endfunction()
