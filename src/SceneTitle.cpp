dc161938153235788d9dc4a25e0279a89084a1b3
mjj1997 <45837504+mjj1997@users.noreply.github.com>
2025-10-21 10:26:43 +0800

    feat: Add bgm to title scene

---
+++ src/SceneTitle.cpp
@@ -0,0 +1,23 @@
+#include "SceneTitle.h"
+
+#include <SDL2/SDL.h>
+
+void SceneTitle::init()
+{
+    // 加载并播放背景音乐
+    m_bgm = Mix_LoadMUS("assets/music/06_Battle_in_Space_Intro.ogg");
+    if (m_bgm == nullptr) {
+        SDL_LogError(SDL_LOG_CATEGORY_ERROR, "Failed to load background music: %s", Mix_GetError());
+    }
+    Mix_PlayMusic(m_bgm, -1);
+}
+
+
+void SceneTitle::clean()
+{
+    // 清理背景音乐
+    if (m_bgm != nullptr) {
+        Mix_HaltMusic();
+        Mix_FreeMusic(m_bgm);
+    }
+}
